9745e2b4e622337dbc8e96fc32e20ffd31a7a21e
as <bezotosniyau@yandex.ru>
2023-02-06 17:08:13 +0300

    1.2. Входные данные в тесте [#297750]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,16 +2,11 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        // System.out.println("Aleksei");
-        // System.out.println("01.12.1990");
-        // 1 выражение
         long l = 129;
         byte b = (byte) l;
         System.out.println("l = " + l);
         System.out.println("b = " + b);
-
         System.out.println();
-        // 2 выражение
         float f = 12358.7f;
         int i = 45981;
         short rsl = (short) (f + i);
@@ -19,13 +14,11 @@ public class StudentInfo {
         System.out.println("i = " + i);
         System.out.println("rsl = " + rsl);
         System.out.println();
-        // 4 выражение
         double d = 121.19;
         byte k = (byte) d;
         System.out.println("d = " + d);
         System.out.println("k = " + k);
         System.out.println();
-        // 3 выражение
         char c = 45000;
         float u = c;
         System.out.println("c = " + c);
