7531a341bbec09a1dbc4244948ea3151ee2ffde4
as <bezotosniyau@yandex.ru>
2023-02-06 15:53:16 +0300

    1.2. Входные данные в тесте [#297750]

---
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -0,0 +1,69 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class X2Test {
+
+    @Test
+    public void whenA10B0C0X2Then40() {
+        /* Входные параметры. Их будет 4 для данного случая*/
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
+        /* Ожидаемое значение. Это всегда одна переменная. */
+        int expected = 40;
+        /* Вызов метода, который хотим проверить. */
+        /* Результат вычисления. Это всегда одна переменная. */
+        int rsl = X2.calc(a, b, c, x);
+        /* сравнение полученного значения с ожидаемым */
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test // 1) a = 1, b = 1, c = 1, x = 1  итог = 3
+    public void whenA1B1C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int rsl1 = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl1);
+    }
+
+    @Test// 2) a = 0, b = 1, c = 1, x = 1.  итог = 2
+    public void whenA0B1C1X1Then2() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int rsl2 = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl2);
+
+    }
+
+    @Test // 3) a = 1, b = 1, c = 0, x = 1.  итог = 2
+    public void whenA1B1C0X1Then2() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int rsl3 = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl3);
+
+    }
+
+    @Test // 4) a = 1, b = 1, c = 1, x = 0 итог = 1
+    public void whenA1B1C1X0Then1() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int rsl4 = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl4);
+    }
+}
