928a7ccafd1135b10035f4bd6b7308869d3e7a53
as <bezotosniyau@yandex.ru>
2023-02-16 22:58:25 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-        System.out.println("Размер массива равен: " + ages.length);
-        System.out.println("Размер массива равен: " + surnames.length);
-        System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Aleksei";
+        names[1] = "Ivan";
+        names[2] = "Sergei";
+        names[3] = "Viktor";
+        System.out.println("Значение индекса массива равно: " + names[0]);
+        System.out.println("Значение индекса массива равно: " + names[1]);
+        System.out.println("Значение индекса массива равно: " + names[2]);
+        System.out.println("Значение индекса массива равно: " + names[3]);
     }
 }
