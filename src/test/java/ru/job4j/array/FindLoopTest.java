527ff9a9744d82b1927da94c85947eef624013d6
as <bezotosniyau@yandex.ru>
2023-02-20 13:05:36 +0300

    6.5. Поиск индекса в диапазоне. [#156318 #393450]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FindLoopTest {
-
     @Test
     public void whenArrayHas5Then0() {
         int[] data = new int[] {5, 10, 3};
@@ -15,29 +14,41 @@ public class FindLoopTest {
     }
 
     @Test
-    public void whenArrayHas44Then0() {
-        int[] data = new int[] {4, 10, 44};
-        int el = 44;
-        int result = FindLoop.indexOf(data, el);
-        int expected = 2;
+    public void whenDiapasonHas8ThenResultEqualFinish() {
+        int[] data = new int[] {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int el = 8;
+        int start = 2;
+        int finish = 5;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenArrayHasNot10ThenMinus1() {
-        int[] data = new int[] {5, 8, 3};
-        int el = 10;
-        int result = FindLoop.indexOf(data, el);
-        int expected = -1;
+    public void whenDiapasonHasSomeEqualElementThen3() {
+        int[] data = new int[] {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        int el = 4;
+        int start = 1;
+        int finish = 8;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenArrayHasNot14ThenMinus1() {
-        int[] data = new int[] {5, 8, 3};
-        int el = 14;
-        int result = FindLoop.indexOf(data, el);
-        int expected = -1;
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8ThenMinus1() {
+        /* Дописать тест, когда элемент 8 в диапазоне не найден.
+        Можно искать любой другой элемент, корректно отражая этот факт в названии теста. */
     }
 }
