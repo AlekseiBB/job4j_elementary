0e21b99a06989528e9cb28afc31af808b0e1317d
as <bezotosniyau@yandex.ru>
2023-02-23 14:07:20 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean result = true;
+        if (name.isEmpty() || (isUpperLatinLetter(name.charAt(0)))
+                || (Character.isDigit(name.charAt(0)))) {
+            return false;
+        }
+        for (int i = 1; i < name.length(); i++) {
+            int symbol = name.charAt(i);
+            result = ((isSpecialSymbol(symbol)) || (isUpperLatinLetter(symbol))
+                    || (isLowerLatinLetter(symbol) || (Character.isDigit(symbol))));
+            if (!result) {
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return (code == 36) || (code == 95);
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+}
